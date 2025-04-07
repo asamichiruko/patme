6a8c3b96b7443c7a4c472097b6c4331110a0c298
Asami Chiruko <asami.chiruko@gmail.com>
2025-04-07 22:45:07 +0900

    タブ切り替え表示の実装

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
-import InputForm from "./components/InputForm.vue"
-import RecordList from "./components/RecordList.vue"
+import { ref } from "vue"
 import { RecordModel } from "./models/RecordModel.js"
 import { Storage } from "./models/Storage.js"
+import InputForm from "./components/InputForm.vue"
+import RecordList from "./components/RecordList.vue"
+import SettingsForm from "./components/SettingsForm.vue"
 
 const recordModel = new RecordModel(new Storage())
+const currentView = ref("InputForm")
 </script>
 
 <template>
-    <InputForm :recordModel="recordModel" />
-    <RecordList :recordModel="recordModel" />
+    <div class="tab-container">
+        <div class="tab" :class="{ active: currentView === 'InputForm' }" @click="currentView = 'InputForm'">入力する</div>
+        <div class="tab" :class="{ active: currentView === 'RecordList' }" @click="currentView = 'RecordList'">リスト</div>
+        <div class="tab" :class="{ active: currentView === 'SettingsForm' }" @click="currentView = 'SettingsForm'">設定</div>
+    </div>
+    <div class="tab-content">
+        <div class="container">
+            <InputForm :record-model="recordModel" v-if="currentView === 'InputForm'" />
+            <RecordList :record-model="recordModel" v-if="currentView === 'RecordList'" />
+            <SettingsForm :record-model="recordModel" v-if="currentView === 'SettingsForm'" />
+        </div>
+    </div>
 </template>
 
 <style scoped>
---
+++ src/components/SettingsForm.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from "vue"
+
+const props = defineProps({
+    recordModel: Object
+})
+
+const exportMessage = ref("")
+const importMessage = ref("")
+const file = ref(null)
+
+function onFileSelected(e) {
+    file.value = e.target.files[0]
+}
+
+function exportRecords() {
+    const jsonString = props.recordModel.exportAsJsonString()
+    const blob = new Blob([jsonString], { type: "application/json" })
+    const url = URL.createObjectURL(blob)
+    const link = document.createElement("a")
+    link.href = url
+    const dateString = new Date().toLocaleDateString("sv-SE")
+    link.download = `records-${dateString}.json`
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    URL.revokeObjectURL(url)
+
+    exportMessage.value = "データをダウンロードしました"
+}
+
+async function importRecords() {
+    if (!file.value || file.value.type !== "application/json") {
+        importMessage.value = ".json形式のファイルを選択してください"
+        return
+    }
+    const jsonString = await file.value.text()
+    const result = props.recordModel.importFromJsonString(jsonString)
+    if (result) {
+        importMessage.value = "データを復元しました"
+    } else {
+        importMessage.value = `データの復元に失敗しました。選択したデータの内容を確認し、時間をおいて再度お試しください`
+    }
+}
+</script>
+
+<template>
+<p>
+    現在ブラウザにあるデータをJSON形式のファイルとしてダウンロードします。保存したファイルを使って後からデータを復元できます。
+</p>
+<p>
+    <button @click="exportRecords">現在のデータをダウンロードする</button>
+</p>
+<p>{{ exportMessage }}</p>
+<p>
+    保存したJSON形式のファイルをアップロードし、ブラウザにデータを復元します。いまブラウザに残っているデータは上書きせず、差分のデータだけを追加します。
+</p>
+<p><input type="file" @change="onFileSelected" accept=".json" /></p>
+<p>
+    <button @click="importRecords">ファイルをアップロードして復元する</button>
+</p>
+<p>{{ importMessage }}</p>
+</template>
+
+<style scoped>
+</style>
