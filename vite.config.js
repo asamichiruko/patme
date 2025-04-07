fcb32b9dffacd1af8895791b0a6cce07619e66bf
Asami Chiruko <asami.chiruko@gmail.com>
2025-04-07 18:07:17 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/pat-self-otb',
+  plugins: [vue()],
+})
