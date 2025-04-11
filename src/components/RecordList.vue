1949250be14c50997a58eabf8e0a7cdcba78eece
Asami Chiruko <asami.chiruko@gmail.com>
2025-04-11 14:26:54 +0900

    Starを追加するモーダルダイアログを実装

--- src/components/RecordList.vue
+++ src/components/RecordList.vue
@@ -1,20 +1,27 @@
 <script setup>
 import { onMounted, ref } from "vue"
+import StarDialog from "@/components/StarDialog.vue"
 
 const props = defineProps({
   recordModel: Object,
 })
 
 const records = ref([])
+const showDialog = ref(false)
+const selectedId = ref(null)
 
-const addStar = (e) => {
-  const content = prompt("コメント（任意）")
-  if (content == null) {
-    return
-  }
+const openStarDialog = (id) => {
+  selectedId.value = id
+  showDialog.value = true
+}
 
-  const achievementId = e.target.getAttribute("achievement-id")
+const handleSubmit = ({ achievementId, content }) => {
   props.recordModel.addStar({ achievementId, content })
+  showDialog.value = false
+}
+
+const handleCancel = () => {
+  showDialog.value = false
 }
 
 onMounted(() => {
@@ -31,22 +38,33 @@ onMounted(() => {
     できたことを記録してみましょう！
   </p>
   <ul v-else>
-    <li class="achievement-item" v-for="r in records" :key="r.achievement.id">
-      <div class="achievement-content">{{ r.achievement.content }}</div>
-      <div class="achievement-date">記録日時: {{ r.achievement.date.toLocaleString() }}</div>
+    <li class="achievement-item" v-for="record in records" :key="record.achievement.id">
+      <div class="achievement-content">{{ record.achievement.content }}</div>
+      <div class="achievement-date">記録日時: {{ record.achievement.date.toLocaleString() }}</div>
       <div class="achievement-actions">
-        <button class="star-button" :achievement-id="r.achievement.id" @click="addStar">
+        <button
+          class="star-button"
+          :achievement-id="record.achievement.id"
+          @click="openStarDialog(record.achievement.id)"
+        >
           ほめる
         </button>
       </div>
-      <template v-if="r.stars.length !== 0">
-        <div class="achievement-star">★ {{ r.stars.length }}</div>
-        <ul class="star-history" v-for="s in r.stars" :key="s.id">
-          <li>{{ s.date.toLocaleString() }}: {{ s.content || "理由を問わず" }}</li>
+      <template v-if="record.stars.length !== 0">
+        <div class="achievement-star">★ {{ record.stars.length }}</div>
+        <ul class="star-history" v-for="star in record.stars" :key="star.id">
+          <li>{{ star.date.toLocaleString() }}: {{ star.content || "理由を問わず" }}</li>
         </ul>
       </template>
     </li>
   </ul>
+
+  <StarDialog
+    :show="showDialog"
+    :achievementId="selectedId"
+    @submit="handleSubmit"
+    @cancel="handleCancel"
+  />
 </template>
 
 <style scoped>
