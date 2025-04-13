b3cb672b6e7f85c40a90020d93cd2144c717bb1c
Asami Chiruko <asami.chiruko@gmail.com>
2025-04-13 15:17:38 +0900

    eslintのエラー解消のため設定ファイルを書き換え

--- eslint.config.js
+++ eslint.config.js
@@ -2,25 +2,33 @@ import { defineConfig } from "eslint/config"
 import js from "@eslint/js"
 import globals from "globals"
 import pluginVue from "eslint-plugin-vue"
-import json from "@eslint/json"
-import css from "@eslint/css"
 import eslintConfigPrettier from "eslint-config-prettier"
+import vitest from "@vitest/eslint-plugin"
 
 export default defineConfig([
   { ignores: ["**/dist/**", "**/docs/**"] },
   { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
   {
     files: ["**/*.{js,mjs,cjs,vue}"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/tests/**"],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+      },
+    },
   },
   pluginVue.configs["flat/essential"],
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  {
-    files: ["**/*.jsonc"],
-    plugins: { json },
-    language: "json/jsonc",
-    extends: ["json/recommended"],
-  },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
   eslintConfigPrettier,
 ])
