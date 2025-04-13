222a9bffb5122ed2e535a284958cfd370278853f
Asami Chiruko <asami.chiruko@gmail.com>
2025-04-13 15:16:53 +0900

    実装に合わせてイベント発火をclickからsumitへ変更

--- tests/components/InputForm.test.js
+++ tests/components/InputForm.test.js
@@ -32,72 +32,72 @@ describe("InputForm.vue", () => {
     vi.restoreAllMocks()
   })
 
-  test("フォームに入力して「記録する」ボタンを押すと recordModel.addAchievement が呼ばれる", async () => {
+  test("テキストを記入して送信すると recordModel.addAchievement が呼ばれる", async () => {
     const textarea = wrapper.find("textarea")
     await textarea.setValue("テスト記録")
 
-    const button = wrapper.find("button")
-    await button.trigger("click")
+    const form = wrapper.find("form")
+    await form.trigger("submit.prevent")
 
     expect(recordModel.addAchievement).toHaveBeenCalledWith({ content: "テスト記録" })
   })
 
-  test("フォームが空の状態で「記録する」ボタンを押すと recordModel.addAchievement が呼ばれない", async () => {
-    const button = wrapper.find("button")
-    await button.trigger("click")
+  test("テキストを記入せずに送信すると recordModel.addAchievement は呼ばれない", async () => {
+    const form = wrapper.find("form")
+    await form.trigger("submit.prevent")
 
     expect(recordModel.addAchievement).not.toHaveBeenCalled()
   })
 
-  test("フォームが空の状態で「記録する」ボタンを押すと error 通知が出る", async () => {
-    const button = wrapper.find("button")
-    await button.trigger("click")
+  test("テキストを記入せずに送信すると error 通知が出る", async () => {
+    const form = wrapper.find("form")
+    await form.trigger("submit.prevent")
 
     expect(trigger).toHaveBeenCalledWith(expect.any(String), "error")
   })
 
-  test("記録が成功するとフォームがクリアされる", async () => {
+  test("記録に成功すると textarea がクリアされる", async () => {
     const textarea = wrapper.find("textarea")
     await textarea.setValue("テスト記録")
     expect(textarea.element.value).toBe("テスト記録")
 
-    const button = wrapper.find("button")
-    await button.trigger("click")
+    const form = wrapper.find("form")
+    await form.trigger("submit.prevent")
 
     expect(textarea.element.value).toBe("")
   })
 
-  test("記録が成功すると success 通知が出る", async () => {
+  test("記録に成功すると success 通知が出る", async () => {
     const textarea = wrapper.find("textarea")
     await textarea.setValue("テスト記録")
 
-    const button = wrapper.find("button")
-    await button.trigger("click")
+    const form = wrapper.find("form")
+    await form.trigger("submit.prevent")
 
     expect(trigger).toHaveBeenCalledWith(expect.any(String), "success")
   })
 
-  test("記録が失敗するとフォームがクリアされない", async () => {
+  test("記録に失敗するとtextarea はクリアされない", async () => {
     recordModel.addAchievement.mockReturnValue(false)
 
     const textarea = wrapper.find("textarea")
     await textarea.setValue("テスト記録")
 
-    const button = wrapper.find("button")
-    await button.trigger("click")
+    const form = wrapper.find("form")
+    await form.trigger("submit.prevent")
 
     expect(recordModel.addAchievement).toHaveReturnedWith(false)
     expect(textarea.element.value).not.toBe("")
   })
 
-  test("記録が失敗すると error 通知が出る", async () => {
+  test("記録に失敗すると error 通知が出る", async () => {
     recordModel.addAchievement.mockReturnValue(false)
 
     const textarea = wrapper.find("textarea")
     await textarea.setValue("テスト記録")
 
-    const button = wrapper.find("button")
-    await button.trigger("click")
+    const form = wrapper.find("form")
+    await form.trigger("submit.prevent")
 
     expect(trigger).toHaveBeenCalledWith(expect.any(String), "error")
   })
