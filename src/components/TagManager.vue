6ce05b4d576e51abafa63dcccb265e25a9dc8870
Asami Chiruko <asami.chiruko@gmail.com>
2025-05-01 12:52:55 +0900

    タグリストのスタイルを設定

--- src/components/TagManager.vue
+++ src/components/TagManager.vue
@@ -35,16 +35,22 @@ watch(
 
 <template>
   <form class="tag-manager" @submit.prevent="saveEdits">
-    <ul class="tag-list">
-      <draggable v-model="reorderedTags" item-key="id" handle=".drag-handle" tag="li">
-        <template #item="{ element }">
-          <li class="tag-list-item">
-            <img class="drag-handle" :src="handleImg" alt="" width="20px" height="20px" />
-            <span class="tag-title">{{ element.title }} / {{ element.order }}</span>
-          </li>
-        </template>
-      </draggable>
-    </ul>
+    <draggable
+      v-model="reorderedTags"
+      item-key="id"
+      handle=".drag-handle"
+      tag="ul"
+      animation="200"
+      ghost-class="ghost"
+      class="tag-list"
+    >
+      <template #item="{ element }">
+        <li class="tag-list-item">
+          <img class="drag-handle" :src="handleImg" alt="" width="20px" height="20px" />
+          <span class="tag-title">{{ element.title }}</span>
+        </li>
+      </template>
+    </draggable>
     <div class="tag-edit-actions">
       <button type="button" class="cancel-button" @click="resetEdits">キャンセル</button>
       <button type="submit" class="primary-button">保存</button>
@@ -54,10 +60,11 @@ watch(
 
 <style scoped>
 .tag-manager {
-  width: 600px;
+  max-width: 600px;
 }
 
 .tag-list {
+  background-color: var(--color-tag-list-bg);
   list-style-type: none;
   padding: 16px;
 
@@ -66,11 +73,13 @@ watch(
   display: flex;
   flex-direction: column;
   margin-bottom: 32px;
-  border: 1px solid var(--color-border);
+  border: 1px solid var(--color-tag-list-border);
   border-radius: 8px;
 }
 .tag-list-item {
-  border-bottom: 1px solid var(--color-border);
+  background-color: var(--color-tag-list-border);
+  color: var(--color-tag-list-text);
+  border-bottom: 1px solid var(--color-tag-list-border);
   padding: 8px 0;
   display: flex;
   gap: 16px;
@@ -81,13 +90,13 @@ watch(
   height: 20px;
   display: inline-block;
 }
-.dragging {
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
 .tag-title {
   display: inline-block;
-  color: var(--color-text);
+  color: var(--color-tag-list-text);
   height: 24px;
+}
+.ghost {
+  background-color: var(--color-tag-list-focus);
 }
 
 .tag-edit-actions {
