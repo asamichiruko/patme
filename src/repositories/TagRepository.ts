4fffdc68d20af9cfdab23346795e9154502361e6
Asami Chiruko <asami.chiruko@gmail.com>
2025-08-11 13:37:26 +0900

    TagRepository を追加

---
+++ src/repositories/TagRepository.ts
@@ -0,0 +1,57 @@
+import type { DataStoreAdapter } from "@/types"
+import { type Tag, TagSchema } from "@/schemas/Tag"
+
+export class TagRepository {
+  constructor(private adapter: DataStoreAdapter<Tag>) {}
+
+  async get(id: string): Promise<Tag | null> {
+    const rawData = await this.adapter.get(id)
+    return rawData ? TagSchema.parse(rawData) : null
+  }
+
+  async getAll(): Promise<Tag[]> {
+    const rawDataArray = await this.adapter.getAll()
+    return rawDataArray.map((rawData) => TagSchema.parse(rawData))
+  }
+
+  async add(tagBody: Omit<Tag, "id" | "createdAt" | "sortOrder">): Promise<string> {
+    const allTags = await this.adapter.getAll()
+    if (allTags.some((t) => t.title === tagBody.title))
+      throw new Error(`Tag title ${tagBody.title} already exists`)
+
+    const maxSortOrder = allTags.reduce((max, tag) => Math.max(max, tag.sortOrder), -1)
+
+    const newTag: Omit<Tag, "id"> = {
+      ...tagBody,
+      createdAt: new Date().toISOString(),
+      sortOrder: maxSortOrder + 1,
+    }
+    return this.adapter.add(newTag)
+  }
+
+  async update(tag: Tag): Promise<void> {
+    const allTags = await this.adapter.getAll()
+    if (allTags.some((existing) => existing.title === tag.title && existing.id !== tag.id))
+      throw new Error(`Tag title ${tag.title} already exists`)
+    this.adapter.update(TagSchema.parse(tag))
+  }
+
+  async updateSortOrders(sortedTags: Tag[]): Promise<void> {
+    const updatedTags = sortedTags.map((tag, index) => ({
+      ...tag,
+      sortOrder: index,
+    }))
+
+    updatedTags.forEach((tag) => TagSchema.parse(tag))
+
+    this.adapter.updateAll(updatedTags)
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.adapter.delete(id)
+
+    const allTags = await this.getAll()
+    allTags.sort((a, b) => a.sortOrder - b.sortOrder)
+    this.updateSortOrders(allTags)
+  }
+}
---
+++ src/repositories/__tests__/TagRepository.test.ts
@@ -0,0 +1,146 @@
+import type { DataStoreAdapter } from "@/types"
+import { TagRepository } from "../TagRepository"
+import { type Tag } from "@/schemas/Tag"
+
+describe("TagRepository", () => {
+  const mockAdapter = {
+    get: vi.fn(),
+    getAll: vi.fn(),
+    add: vi.fn(),
+    update: vi.fn(),
+    updateAll: vi.fn(),
+    delete: vi.fn(),
+  } satisfies DataStoreAdapter<Tag>
+
+  let repo: TagRepository
+
+  beforeEach(() => {
+    repo = new TagRepository(mockAdapter)
+    vi.clearAllMocks()
+  })
+
+  test("全 tag を取得できる", async () => {
+    const tag1 = {
+      id: "id1",
+      createdAt: new Date().toISOString(),
+      title: "title 1",
+      sortOrder: 0,
+    }
+    const tag2 = {
+      id: "id2",
+      createdAt: new Date().toISOString(),
+      title: "title 2",
+      sortOrder: 1,
+    }
+    mockAdapter.getAll.mockResolvedValue([tag1, tag2])
+    const tags = await repo.getAll()
+    expect(tags[0].id).toEqual(tag1.id)
+    expect(tags[1].id).toEqual(tag2.id)
+  })
+
+  test("存在する tag を取得できる", async () => {
+    const existing = {
+      id: "id1",
+      createdAt: new Date().toISOString(),
+      title: "title 1",
+      sortOrder: 0,
+    }
+    mockAdapter.get.mockResolvedValue(existing)
+    const tag = await repo.get("id1")
+    expect(tag!.id).toBe(existing.id)
+    expect(tag!.createdAt).toBe(existing.createdAt)
+    expect(tag!.title).toBe(existing.title)
+    expect(tag!.sortOrder).toBe(existing.sortOrder)
+  })
+
+  test("取得しようとした id が存在しなかった場合 null が返る", async () => {
+    mockAdapter.get.mockResolvedValue(null)
+    const tag = await repo.get("id1")
+    expect(tag).toBeNull()
+  })
+
+  test("新規 tag を追加できる", async () => {
+    mockAdapter.getAll.mockResolvedValue([
+      {
+        id: "id1",
+        createdAt: new Date().toISOString(),
+        title: "title 1",
+        sortOrder: 0,
+      },
+    ])
+
+    mockAdapter.add.mockResolvedValue("id2")
+    const tagBody: Omit<Tag, "id" | "createdAt" | "sortOrder"> = {
+      title: "title 2",
+    }
+    const id = await repo.add(tagBody)
+    expect(mockAdapter.add).toHaveBeenCalledWith({
+      ...tagBody,
+      createdAt: expect.any(String),
+      sortOrder: 1,
+    })
+    expect(id).toBe("id2")
+  })
+
+  test("既存 tag を更新できる", async () => {
+    const oldTag: Tag = {
+      id: "id1",
+      createdAt: new Date().toISOString(),
+      title: "old title",
+      sortOrder: 0,
+    }
+    const newTag: Tag = {
+      ...oldTag,
+      title: "new title",
+    }
+
+    mockAdapter.getAll.mockResolvedValue([oldTag])
+    await repo.update(newTag)
+    expect(mockAdapter.update).toHaveBeenCalledWith(newTag)
+  })
+
+  test("sortOrder を振り直せる", async () => {
+    const tag1 = {
+      id: "id1",
+      createdAt: new Date().toISOString(),
+      title: "title 1",
+      sortOrder: 0,
+    }
+    const tag2 = {
+      id: "id2",
+      createdAt: new Date().toISOString(),
+      title: "title 2",
+      sortOrder: 1,
+    }
+
+    await repo.updateSortOrders([tag2, tag1])
+    expect(mockAdapter.updateAll).toHaveBeenCalledWith([
+      { ...tag2, sortOrder: 0 },
+      { ...tag1, sortOrder: 1 },
+    ])
+  })
+
+  test("tag を削除できる", async () => {
+    const tag1 = {
+      id: "id1",
+      createdAt: new Date().toISOString(),
+      title: "title 1",
+      sortOrder: 0,
+    }
+    // tag2 will delete
+    const tag3 = {
+      id: "id3",
+      createdAt: new Date().toISOString(),
+      title: "title 3",
+      sortOrder: 2,
+    }
+    mockAdapter.getAll.mockResolvedValue([tag1, tag3])
+
+    await repo.delete("id2")
+    expect(mockAdapter.delete).toHaveBeenCalledWith("id2")
+    expect(mockAdapter.updateAll).toHaveBeenCalledWith([
+      { ...tag1, sortOrder: 0 },
+      { ...tag3, sortOrder: 1 },
+    ])
+  })
+})
