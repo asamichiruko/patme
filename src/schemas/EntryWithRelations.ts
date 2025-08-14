8a04f8892e167a5497a316b5434fc880318b7dc9
Asami Chiruko <asami.chiruko@gmail.com>
2025-08-14 23:45:21 +0900

    EntryWithRelations 型のスキーマを作成

--- src/schemas/EntryWithRelations.ts
+++ src/schemas/EntryWithRelations.ts
@@ -1,8 +1,11 @@
-import type { Entry } from "@/schemas/Entry"
-import type { Comment } from "@/schemas/Comment"
-import type { Tag } from "@/schemas/Tag"
+import { z } from "zod"
+import { EntrySchema } from "./Entry"
+import { CommentSchema } from "./Comment"
+import { TagSchema } from "./Tag"
 
-export type EntryWithRelations = Entry & {
-  comments: Comment[]
-  tags: Tag[]
-}
+export const EntryWithRelationsSchema = EntrySchema.extend({
+  comments: z.array(CommentSchema),
+  tags: z.array(TagSchema),
+})
+
+export type EntryWithRelations = z.infer<typeof EntryWithRelationsSchema>
